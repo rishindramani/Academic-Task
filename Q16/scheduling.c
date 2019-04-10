88297d2d35210e7028aba4580d506a4cc8d6d4dc
Rishindra Mani <rishindramani@outlook.com>
2019-04-10 23:07:57 +0530

    Update scheduling.c

--- Q16/scheduling.c
+++ Q16/scheduling.c
@@ -1,65 +1,130 @@
 #include<stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-int main() 
-{ 
-  int c,j,pro;//variable for count and number of processes
-  int ti,re;// time and process variables
-  int check=0;//flag variable
-  int quan=10; //time quantum=10 given
-  int wt=0,tat=0,at[10],bt[10],rt[10]; //initialising waiting time,turnaround time,arrival time,burst time
-  
-  printf("\n\nEnter Total Processes:\t "); 
-  scanf("%d",&pro); 
-  re=pro;
-   
-  for(c=0;c<pro;c++) 
-  { 
-    printf("Enter Arrival Time and Burst Time for Process %d :",c+1); 
-    scanf("%d",&at[c]); 
-    scanf("%d",&bt[c]); 
-    rt[c]=bt[c]; 
-  } 
-  printf("Time Quantum= %d time units\t",quan); 
+#include<unistd.h>
+#include<stdlib.h>
+struct Pro
+{
+int prono; //struct definition
+int arrt; 
+int burt; 
+};
+int main()
+{
 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-  for(ti=0,c=0;re!=0;) 
-  { 
-    if(rt[c]<=quan && rt[c]>0) //loop ealuation from remaining time in the queue
-    { 
-      ti+=rt[c]; 
-      rt[c]=0; 
-      check=1; 
-    } 
-    else if(rt[c]>0)
-    { 
-      rt[c]-=quan;                   //preempting on the basis of time quanta scheduling processes having short execution time first.
-      ti+=quan; 
-    } 
-    if(rt[c]==0 && check==1) 
-    { 
-      re--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",c+1,ti-at[c],ti-at[c]-bt[c]); 
-      wt+=ti-at[c]-bt[c]; 
-      tat+=ti-at[c]; 
-      check=0; 
-    } 
-    if(c==pro-1) 
-    {
-	
-      c=0; 
-    }
-	else if(at[c+1]<=ti) 
-    {
-	   c++; 
-    }
-	else 
-    {
-	   c=0; 
+int tp; //Total Processes to be encountered
+printf("\nEnter the Total Number of Processes:\n");//process initiaisation
+scanf("%d",&tp);
+struct Pro p[tp];
+
+
+int minart; //to compute minimum arrival time
+int t1=0; //Time variable
+int l1,l2; //Loop variables used in sorting and other functions
+float totalwaitt=0; //to compute Total Waiting Time
+float totalturnt=0; //to compute Total Turnaround Time 
+float avgwt; //to compute Average Waiting Time
+float avgturnt; //to compute Average Turnaround Time
+
+//DETAILS OF THE PROCESS
+int burst[tp];// to store burst time
+printf("Enter Details of %d Processes\n\n",tp);
+for(l1=0;l1<tp;l1++)
+{
+printf("Enter the process id number: \t");
+scanf("%d",&p[l1].prono);
+printf("Enter Arrival Time:\t");
+scanf("%d",&p[l1].arrt);
+printf("Enter Burst Time: \t");
+scanf("%d",&p[l1].burt);
+burst[l1]=p[l1].burt;
+printf("\n");
 }
-  } 
-  printf("\nAverage Waiting Time= %f\n",wt*1.0/pro); //printing the average waiting time and average turnaround time
-  printf("Average Turnaround Time = %f",tat*1.0/pro); 
 
-  return 0; 
+int te;
+for(l1=0;l1<tp-1;l1++)
+{
+for(l2=0;l2<tp-l1-1;l2++)
+{
+if(p[l2].burt>p[l2+1].burt)  //Sorting on the basis burst time
+{
+te=p[l2].prono;
+p[l2].prono=p[l2+1].prono;
+p[l2+1].prono=te;
+te=p[l2].arrt;
+p[l2].arrt=p[l2+1].arrt;
+p[l2+1].arrt=te;
+te=p[l2].burt;
+p[l2].burt=p[l2+1].burt;
+p[l2+1].burt=te;
+te=burst[l2];
+burst[l2]=burst[l2+1];
+burst[l2+1]=te;
+}
+}
+}
+
+//DISPLAYING THE DETAILS OF THE PROCESSES ENTERED
+printf("******OUTPUT******");
+printf("\nPROCESS NO\t\tARRIVAL TIME\t\t\tBURST TIME\t\n");
+for(l1=0;l1<=tp-1;l1++)
+{
+printf("%d \t\t\t%d \t\t\t\t%d \n\n",p[l1].prono,p[l1].arrt,p[l1].burt);
+}
+
+//ASSIGNING THE TIME VARIABLE THE MINIMUM ARRIVAL TIME
+minart=p[0].arrt;  //Minimum Arrival Time = First Process Arrival Time
+for(l1=1;l1<tp;l1++)
+{
+if(p[l1].arrt<minart){
+minart=p[l1].arrt;
+}
+t1=minart;
+}
+
+
+printf("\n\n********\n\n");
+printf("Process\t|TurnAround-time|Waiting-time\t\n");
+int c=0; //to count No. of process that finished execution
+int flag=0; //to know if Process executed/printed variable
+int timequanta=10; //to store Time Quantum of the processes
+int wt[tp]; //computing waiting time
+int tt[tp]; //computing turnaround time
+for(l1=0;c!=tp;l1++)
+{
+	
+if(p[l1].arrt<=t1 && p[l1].burt<=timequanta && p[l1].burt>0) //verify that Burst Time<Time Quantum
+{
+t1+=p[l1].burt; //time updation
+p[l1].burt=0; //update value of burst time
+flag=1; //to demonstrate that process is executed
+}
+else if(p[l1].arrt<=t1 && p[l1].burt>0)
+{
+p[l1].burt-=timequanta;  
+t1+=timequanta;
+}
+if(p[l1].burt==0 && flag==1)
+{
+c++;
+tt[l1]=t1-p[l1].arrt;
+wt[l1]=t1-p[l1].arrt-burst[l1];
+printf("%d\t|\t%d\t|\t%d\n",p[l1].prono,tt[l1],wt[l1]);
+flag=0; //to check if process printed
+}
+if(l1==tp-1)
+{
+l1=-1;
+}
+}
+printf("\n*****\n");
+for(l1=0;l1<tp;l1++)
+{
+totalwaitt+=wt[l1];
+totalturnt+=tt[l1];
+}
+avgwt=totalwaitt*1.0/tp;
+avgturnt=totalturnt*1.0/tp;
+printf("\nAverage Waiting Timeof the processes= %f\n",avgwt);
+printf("\n\n");
+printf("Average Turnaround Time of the processes= %f",avgturnt);
+printf("\n");
 }
